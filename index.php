51ad7a3cf289473e52369f8a0068f45cbfbc5560
Yamid Pico <ypicoleal@gmail.com>
2017-03-26 15:39:27 -0500

    cambios menores

--- index.php
+++ index.php
@@ -58,10 +58,10 @@
                             <a href="<?php echo get_home_url(); ?>" class="opcion1"></a>
                         </li>
                         <li>
-                            <a href="<?php echo get_option('amigos_menu_url'); ?>" class="opcion2"></a>
+                            <a href="<?php echo get_option('festival_menu_url'); ?>" class="opcion2"></a>
                         </li>
                         <li>
-                            <a href="<?php echo get_option('festival_menu_url'); ?>" class="opcion3"></a>
+                            <a href="<?php echo get_option('amigos_menu_url'); ?>" class="opcion3"></a>
                         </li>
                         <li>
                             <a href="<?php echo get_option('eurofilmpedia_menu_url'); ?>" class="opcion4"></a>
